41ae6b5cb63a02bd2dd46bd47d4b5493e51431af
Gianna Dubinski <167164944+giadubinski@users.noreply.github.com>
2024-11-06 22:31:12 -0600

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Gianna Dubinski / COMP 272-001
  *
  *   This java file contains the problem solutions of isSubSet, findKthLargest,
  *   and sort2Arrays methods. You should utilize the Java Collection Framework for
